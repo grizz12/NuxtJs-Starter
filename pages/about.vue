1822a5fa80d77b0b0dfbaf7a263e0f4d6ffe6137
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-03 16:14:21 +0700

    add about pages

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container" id="about-body">
+        <Navbar />
+        <AboutImg />
+        <span class="line"></span>
+        <AboutContent />
+        <Footer />
+    </div>
+</template>
+
+<style>
+span {
+  display: block;
+  width: 80;
+  height: 1px;
+  background-color: black;
+  margin-top: 5rem;
+}
+</style>
