2d7ba253a4c6aa67a831226aad6ef1d7c66b21b1
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-11-03 16:13:47 +0700

    add about content

---
+++ components/AboutContent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="text-list">
+        <p>Lorem Lorem</p>
+        <h2>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, enim. <br> Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+    </div>
+    <b-container fluid class="about-pathner">
+  <b-row>
+    <b-col>
+      <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+    </b-col>
+    <b-col>
+      <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+    </b-col>
+  </b-row>
+</b-container>
+    </div>
+</template>
+
+<style>
+.about-pathner {
+    margin-left: 3rem;
+    margin-top: 5rem;
+}
+</style>
---
+++ components/AboutImg.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about-img">
+  <b-img src="../assets/intro.svg" fluid alt="Responsive image"></b-img>
+</div>
+</template>
+
+<style>
+.about-img {
+    margin-top: 5rem;
+    margin-left: 11rem;
+}
+</style>
